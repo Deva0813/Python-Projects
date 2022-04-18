a265864675d926d154b25ddaa035975581acfee1
Deva <90745606+Deva0813@users.noreply.github.com>
2022-04-18 20:14:07 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -16,7 +16,10 @@ def mul(n1, n2):
 def div(n1, n2):
     return n1/n2
 
-
+oper = {"+": add,
+        "-": sub,
+        "*": mul,
+        "/": div}
 clear()
 con = 0
 while True:
@@ -46,20 +49,24 @@ while True:
         l_num = int(input("\nEnter the last numebr : "))
         print(f"\n{f_num} {op} {l_num}\n")
 
-        if op == '+':
-            ans = add(f_num, l_num)
-            print(ans)
-        elif op == '-':
-            ans = sub(f_num, l_num)
-            print(ans)
-        elif op == '*':
-            ans = mul(f_num, l_num)
-            print(ans)
-        elif op == '/':
-            ans = div(f_num, l_num)
-            print(ans)
-        else:
-            print("Invalid operator...")
+#         if op == '+':
+#             ans = add(f_num, l_num)
+#             print(ans)
+#         elif op == '-':
+#             ans = sub(f_num, l_num)
+#             print(ans)
+#         elif op == '*':
+#             ans = mul(f_num, l_num)
+#             print(ans)
+#         elif op == '/':
+#             ans = div(f_num, l_num)
+#             print(ans)
+#         else:
+#             print("Invalid operator...")
+
+        func = oper[op]
+        print(func(f_num, l_num))
+        ans = int(func(f_num, l_num))
 
         rep = input(
             f"""\nDo you want to perform any other operation with {ans},
